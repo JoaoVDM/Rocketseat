a1292c0e0864c4966960a3002001f2f5097e43d5
João Vitor Domene Maganini <102229903+JoaoVDM@users.noreply.github.com>
2024-02-10 19:22:59 -0300

    Add files via upload

---
+++ Explorer/Projeto-08/comentarios.js
@@ -0,0 +1,11 @@
+// Aqui eu tenho um comentário em linha
+
+/* 
+    Aqui eu tenho um comentário multi linha
+
+    O código abaixo escreve alguma mensagem no devtools
+
+*/
+
+console.log('Bem vindos ao Starter');
+console.log('Não iria ignorar');
